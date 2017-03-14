5d1c841909efaf2244c632a98918d9fe3bee6db2
Kevin Sawicki <kevinsawicki@gmail.com>
2017-03-14 13:32:04 -0700

    Upgrade libcc for heap snapshot patch

--- script/lib/config.py
+++ script/lib/config.py
@@ -9,7 +9,7 @@ import sys
 BASE_URL = os.getenv('LIBCHROMIUMCONTENT_MIRROR') or \
     'https://s3.amazonaws.com/github-janky-artifacts/libchromiumcontent'
 LIBCHROMIUMCONTENT_COMMIT = os.getenv('LIBCHROMIUMCONTENT_COMMIT') or \
-    '4f5b89374df7ee69095b9f7d50b30fb46ddd7407'
+    '084f710875ab5124e3a88e5c1aabcb8399a01da5'
 
 PLATFORM = {
   'cygwin': 'win32',
